d5136e78ce93789f6ad3f0bb613d03c747011a77
parth-btpl <parth.patel@botreetechnologies.com>
2020-05-10 13:40:15 +0530

    Correct syntax.

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -24,7 +24,7 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
   private
 
   def user_params
-    params.reqire(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
   def ensure_params_exist
